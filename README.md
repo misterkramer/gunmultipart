c3bc67a219516b87338ebb2c71e1beee1e9d71f2
Сергей Д. Крамер <kramer@yamoney.ru>
2018-12-08 03:59:33 +0300

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# Кастомная пушка на основе Pandora
+
+Исходники [Pandora](https://github.com/yandex/pandora/)
+
+[![Read the Docs](https://readthedocs.org/projects/yandexpandora/badge/)](https://readthedocs.org/projects/yandexpandora/)
+
+## С чего начать
+
+### Установить GO
+[Install](https://golang.org/doc/install) Golang >= 1.8.3.
+
+### Скачать Pandora
+```bash
+go get github.com/yandex/pandora
+```
+
+### Скачать и установить multipart_pandora
+Просто переместите папку multipart_pandora в $GOPATH/src/github.com/yandex/pandora/examples/
+Выполните:
+```bash
+cd $GOPATH/src/github.com/yandex/pandora/examples/multipart_pandora
+go get .
+```
+После этого появится исполняемый файл с названием multipart_pandora в $GOPATH/bin
+
+Запустите стрельбу:
+```bash
+multipart_pandora custom.yaml
+```
---
+++ multipart_pandora/custom_main.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"mime/multipart"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/spf13/afero"
+	"github.com/yandex/pandora/cli"
+	"github.com/yandex/pandora/components/phttp/import"
+	"github.com/yandex/pandora/core"
+	"github.com/yandex/pandora/core/import"
+	"github.com/yandex/pandora/core/register"
+)
+
+type Ammo struct {
+	URL  string
+	Path string
+}
+
+type Sample struct {
+	URL              string
+	ShootTimeSeconds float64
+}
+
+type GunConfig struct {
+	Target string `validate:"required"` // Configuration will fail, without target defined
+}
+
+type Gun struct {
+	// Configured on construction.
+	client http.Client
+	conf   GunConfig
+	// Configured on Bind, before shooting
+	aggr core.Aggregator // May be your custom Aggregator.
+	core.GunDeps
+}
+
+func NewGun(conf GunConfig) *Gun {
+	return &Gun{conf: conf}
+}
+
+func (g *Gun) Bind(aggr core.Aggregator, deps core.GunDeps) error {
+	g.client = http.Client{} //keep-alive shooting
+	g.aggr = aggr
+	g.GunDeps = deps
+	return nil
+}
+
+func (g *Gun) Shoot(ammo core.Ammo) {
+	customAmmo := ammo.(*Ammo) // Shoot will panic on unexpected ammo type. Panic cancels shooting.
+	g.shoot(customAmmo)
+}
+
+func newfileUploadRequest(uri string, paramName, path string) (*http.Request, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	fileContents, err := ioutil.ReadAll(file)
+	if err != nil {
+		return nil, err
+	}
+	fi, err := file.Stat()
+	if err != nil {
+		return nil, err
+	}
+	file.Close()
+
+	body := new(bytes.Buffer)
+	writer := multipart.NewWriter(body)
+	part, err := writer.CreateFormFile(paramName, fi.Name())
+	if err != nil {
+		return nil, err
+	}
+	part.Write(fileContents)
+
+	err = writer.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	request, err := http.NewRequest("POST", uri, body)
+	request.Header.Add("Content-Type", writer.FormDataContentType())
+	return request, err
+}
+
+func (g *Gun) shoot(ammo *Ammo) {
+	path, _ := os.Getwd()
+	path += "/default.yml"
+	request, err := newfileUploadRequest(ammo.URL, "file", ammo.Path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	resp, err := g.client.Do(request)
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		var bodyContent []byte
+		fmt.Println(resp.StatusCode)
+		fmt.Println(resp.Header)
+		resp.Body.Read(bodyContent)
+		resp.Body.Close()
+		fmt.Println(bodyContent)
+	}
+	defer func() {
+		start := time.Now()
+		g.aggr.Report(Sample{ammo.URL, time.Since(start).Seconds()})
+	}()
+}
+
+func main() {
+	//debug.SetGCPercent(-1)
+	// Standard imports.
+	fs := afero.NewOsFs()
+	coreimport.Import(fs)
+	// May not be imported, if you don't need http guns and etc.
+	phttp.Import(fs)
+
+	// Custom imports. Integrate your custom types into configuration system.
+	coreimport.RegisterCustomJSONProvider("multipart_provider", func() core.Ammo { return &Ammo{} })
+
+	register.Gun("multipart_gun", NewGun, func() GunConfig {
+		return GunConfig{
+			Target: "default target",
+		}
+	})
+
+	cli.Run()
+}
